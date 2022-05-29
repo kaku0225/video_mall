c9e29279719c3e04a646ad42f197346b327ef578
Eason <acontentedmind@gmail.com>
2022-05-30 00:04:44 +0800

    spec: fix rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'insert user' do
-    let(:params) { { name: 'test', email: 'test@test.com', password: '123456', password_confirmation: '123456' } }
+    let(:params) { { name: 'test', email: 'test@test.com', password: '123456' } }
     context 'success' do
       it 'if parameter correct' do
         User.create(params)
@@ -21,7 +21,7 @@ RSpec.describe User, type: :model do
 
       it 'if email is empty' do
         params[:email] = nil
-        User.create(name: 'test', password: '123456', password_confirmation: '123456')
+        User.create(name: 'test', password: '123456')
         expect(User.count).to eq(0)
       end
 
@@ -46,12 +46,6 @@ RSpec.describe User, type: :model do
 
       it 'if password length is NOT enougth' do
         params[:password] = '123'
-        User.create(params)
-        expect(User.count).to eq(0)
-      end
-
-      it 'if password_confirmation is empty' do
-        params[:password_confirmation] = nil
         User.create(params)
         expect(User.count).to eq(0)
       end
